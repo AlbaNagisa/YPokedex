7336d18b6a1ea0c9121e8b9236a1405aae15ba9f
vmathis <mathis.vassy@ynov.com>
2024-05-06 01:36:54 +0200

    choose only 1 by ID

---
+++ src/app/api/moves/route.ts
@@ -0,0 +1,55 @@
+
+import { pagination } from "@/lib/pagination";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+import { version } from "os";
+import { group } from "console";
+
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+    const url = new URL(req.url)
+
+    const pageString = url.searchParams.get("page")
+    const reqPage = parseInt(pageString ? pageString : "0")
+
+    const elemPerPage = url.searchParams.get("elemPerPage")
+    const reqElemPerPage = parseInt(elemPerPage ? elemPerPage : "5")
+
+    return NextResponse.json(await pagination(reqElemPerPage, reqPage, "pokemon_v2_pokemon", {
+        pokemon_v2_pokemonform: {
+            include: {
+                pokemon_v2_pokemonformgeneration: {
+                    include: {
+                        pokemon_v2_generation: true
+                    }
+                }
+            }
+        },
+        pokemon_v2_pokemonstat: {
+            include: {
+                pokemon_v2_stat: true
+            }
+        },
+
+        pokemon_v2_pokemonability: {
+            include: {
+                pokemon_v2_ability: true
+
+            }
+        },
+
+
+        pokemon_v2_pokemonmove: {
+            where: {
+                version_group_id: 1
+            },
+            include: {
+                pokemon_v2_move: true
+
+            }
+        }
+    }))
+}
+
--- src/app/api/pokemons/[id]/route.ts
+++ src/app/api/pokemons/[id]/route.ts
@@ -1,13 +1,27 @@
-import {NextRequest, NextResponse} from "next/server";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 
 // Pour declencher cette route tu vas a /api/pokemons/1 par exemple pour chopper bulbizarre tu dois choper le pokemon en fonction de l'id qui est dans le console.log
-export const GET = async (req: NextRequest, {params}: { params: { id: string } }) => {
+export const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
+    const url = new URL(req.url)
     console.log(params.id)
-    return NextResponse.json({monPokemon: {}})
+    const pokemonId = params.id !== null && params.id !== undefined ? parseInt(params.id) : null
 
+    if (pokemonId !== null) {
+        return NextResponse.json(await getPokemonById(pokemonId))
+    } else {
+        return NextResponse.json({ error: 'Invalid Pokemon ID' })
+    }
 }
 
+async function getPokemonById(id: number) {
+    return await prisma.pokemon_v2_pokemon.findFirst({
+        where: {
+            id: id
+        }
+    })
+}
 
 // export default function SearchBar() {
 //     const searchParams = useSearchParams()
